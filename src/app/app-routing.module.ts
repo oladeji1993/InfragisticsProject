c508ec9473238ed3e935c199263d302a1d517b09
thrifitdev <developer@thrifit.com>
2022-04-08 16:55:02 +0100

    first commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,39 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { Grid2Component } from './components/grid2/grid2.component';
+import { Grid3Component } from './components/grid3/grid3.component';
+import { Grid4Component } from './components/grid4/grid4.component';
+import { HomeComponent } from './components/home/home.component';
+import { NewGridComponent } from './newgrid/newgrid.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    redirectTo: 'Grid1',
+    pathMatch: 'full'
+  },
+  {
+    path: 'Grid1',
+    component: HomeComponent
+  },
+  {
+    path: 'Grid2',
+    component: Grid2Component
+  },
+  {
+    path: 'Grid3',
+    component: Grid3Component
+  },
+  {
+    path: 'Grid4',
+    component: Grid4Component
+  },
+  { path: 'newgrid', component: NewGridComponent, data: { text: 'newGrid' } }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {
+}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,16 +3,33 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './components/home/home.component';
+import { Grid2Component } from './components/grid2/grid2.component';
+import { Grid3Component } from './components/grid3/grid3.component';
+import { HeadNavComponent } from './components/head-nav/head-nav.component';
+import { Grid4Component } from './components/grid4/grid4.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { NewGridComponent } from './newgrid/newgrid.component';
+import { IgxGridModule } from 'igniteui-angular';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    Grid2Component,
+    Grid3Component,
+    HeadNavComponent,
+    Grid4Component,
+    NewGridComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    IgxGridModule
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+}
